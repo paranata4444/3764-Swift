9e03713361b0f145eec9717682b748a11badc690
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-11-27 15:00:06 +0200

    init

---
+++ BirdsPen/App/BirdsPenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdsPenApp.swift
+//  BirdsPen
+//
+//  Created by Serhii Babchuk on 27.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BirdsPenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BPLaunchView()
+        }
+    }
+}
